99caa4a8100dd1da3d1055b2723ff05f5e17f9bc
George <drpresq@users.noreply.github.com>
2022-01-04 13:24:41 -0500

    Delete h-stats.sh

--- h-stats.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-stats_raw=`curl --connect-timeout 2 --max-time 5 --silent --noproxy '*' http://127.0.0.1:60050`
-
-khs=`echo $stats_raw | jq -r '.hashrate.total[0]'`
-stats=`echo $stats_raw | jq '{hs: [.hashrate.threads[][0]], hs_units: "khs", uptime: .connection.uptime, $
-
-#echo $khs
-#echo $stats
